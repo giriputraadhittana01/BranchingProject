67d0b52c36e51cc37b2984d3f7a15bc44ba3a033
Giri Putra Adhittana <giri.adhittana@tokopedia.com>
2022-04-26 23:13:05 +0700

    add feature b

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 	fmt.Println("Boba Hello")
-	fmt.Println("Feature A B C")
-	fmt.Println("Feature D E F")
-	fmt.Println("Feature G H I")
-	fmt.Println("Feature J K L")
+	fmt.Println("Feature A")
+	fmt.Println("Feature B")
 }
