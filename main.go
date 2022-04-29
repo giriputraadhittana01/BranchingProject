3d966a0296abd5f07c0d307f36c102a4b2d5ed0a
giriadhittana01 <103641337+giriadhittana01@users.noreply.github.com>
2022-04-29 14:54:20 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,9 +3,6 @@ package main
 import "fmt"
 
 func main() {
-
-	fmt.Println("Hello World")
-	fmt.Println("Boba Hello World")
 	fmt.Println("Feature A Great Doda")
 	fmt.Println("Feature B Great")
 	fmt.Println("Feature C Great")
