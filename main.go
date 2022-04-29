b3cd1ea04015579a05ae261ab98aac667808c01c
Giri Putra Adhittana <giri.adhittana@tokopedia.com>
2022-04-29 14:29:57 +0700

    Resolve conflict

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Feature A")
+	fmt.Println("Hello World")
+	fmt.Println("Boba Hello World")
+	fmt.Println("Feature A Great")
 	fmt.Println("Feature B")
 	fmt.Println("Feature C")
 	fmt.Println("Feature D")
